106dae5220ee38910bac8d4d87231778b0baf78b
Anishya Thinesh <amt2622@rit.edu>
2023-10-26 16:00:04 -0400

    main file

---
+++ main.py
@@ -0,0 +1,14 @@
+import subprocess
+import sys
+
+if __name__ == "__main__":
+    # run landing login/signup
+    subprocess.run([sys.executable, 'landing.py'])
+
+    # run home page
+
+
+
+
+
+    print("main finished")
