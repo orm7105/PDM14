c16c82492b58bc42d35d5e6688351262ebf118fc
jz2447 <jz2447@rit.edu>
2023-11-22 23:16:25 -0500

    fixed the plays so that it properly adds all the times for each song and their artist. Also updated the print statements to look a little nicer. also made it so the user can keep asking to see their top artists until they are content.

--- user_info.py
+++ user_info.py
@@ -9,6 +9,73 @@ password = sensitive.get_pass()
 dbName = "p320_14"
 
 
+def top_plays(listener):
+    print("Here are your top 10 artists by the most plays:")
+    # get the top 10 most played songs, using those songids, get the artist from them
+    curs.execute(f"""
+                    SELECT S.artist,
+                        SUM(EXTRACT(HOUR FROM date_time) * 3600 +
+                            EXTRACT(MINUTE FROM date_time) * 60 +
+                            EXTRACT(SECOND FROM date_time)) AS total_time_played
+                    FROM listeners_listensto_song L
+                    JOIN song S on L.songid = S.songid
+                    WHERE L.userid = %s
+                    GROUP BY S.artist
+                    ORDER BY total_time_played DESC
+                    LIMIT 10
+                """, (listener,))
+    top_songs_played = curs.fetchall()
+    i = 0
+    for songs in top_songs_played:
+        i += 1
+        artists = songs[0]
+        print(f"\t{i}. {artists}")
+
+
+
+def top_additions(listener):
+    # get the top 10 artists by most additions to playlists
+    print("Here are your top 10 artists by the most addition to playlists:")
+    # get all the playlists owned by a user
+    curs.execute(f"""
+                SELECT playlistid
+                FROM listeners_owns_playlist
+                WHERE userid = %s
+            """, (listener,))
+    playlists = curs.fetchall()
+    # get all the songids in those playlists
+
+    # dictionary to store artist counts
+    artist_counts = {}
+
+    for playlist in playlists:
+
+        curs.execute(f"""
+                    SELECT songid
+                    FROM playlist_has_song
+                    WHERE playlistid = %s
+                """, (playlist,))
+        songids = curs.fetchall()
+        for songid in songids:
+            curs.execute(f"""
+                        SELECT artist
+                        FROM SONG
+                        WHERE songid = %s
+                    """, (songid,))
+            artist = curs.fetchone()
+
+            # counts instances of artists in the dictionary
+            if artist in artist_counts:
+                artist_counts[artist] += 1
+            else:
+                artist_counts[artist] = 1
+
+    # does the sorting to get the top ten
+    top_artists = sorted(artist_counts.items(), key=lambda x: x[1], reverse=True)[:10]
+    j = 0
+    for artist, count in top_artists:
+        j += 1
+        print(f"\t{j}. {artist[0]}: {count} additions to playlists")
 
 
 try:
@@ -44,7 +111,7 @@ try:
             first = user_name[0]
             last = user_name[1]
 
-            print(f"Hi {first} {last}! Welcome to your profile page!")
+            print(f"Hi {first} {last}! Welcome to your profile page!\n")
 
             curs.execute(f"""
                         SELECT COUNT(playlistid)
@@ -54,7 +121,7 @@ try:
 
             collection_count = curs.fetchone()
             collect = collection_count[0]
-            print(f"You currently have {collect} playlists.")
+            print(f"You currently have {collect} {'playlist' if collect == 1 else 'playlists'}.")
             # number of subscribers you have
             # get the count of that user id from followingid
             curs.execute(f"""
@@ -65,7 +132,7 @@ try:
 
             follower_count = curs.fetchone()  # Fetch all matching rows
             follower = follower_count[0]
-            print(f"You have {follower} followers!")
+            print(f"You have {follower} {'follower' if follower == 1 else 'followers'}!")
             # number of people you subscribe to
             # get the count of user id from followerid
             curs.execute(f"""
@@ -76,86 +143,29 @@ try:
 
             following_count = curs.fetchone()  # Fetch all matching rows
             following = following_count[0]
-            print(f"You are following {following} users!")
+            print(f"You are following {following} {'user' if following == 1 else 'users'}!\n")
 
-            # the top 10 artists by most plays
-            print("Here are your top 10 artists by the most plays:")
-            # get the top 10 most played songs, using those songids, get the artist from them
-            curs.execute(f"""
-                        SELECT songid
-                        FROM listeners_listensto_song
-                        WHERE userid = %s
-                        ORDER BY date_time DESC
-                        LIMIT 10
-                    """, (listener_id,))
-            top_songs_played = curs.fetchall()
-            i = 0
-            for songs in top_songs_played:
-                i += 1
-                curs.execute(f"""
-                            SELECT artist
-                            FROM song
-                            WHERE songid = %s
-                        """, (songs,))
-                a = curs.fetchone()
-                artist = a[0]
-                print(f"\t{i}. {artist}")
-            p = 0
-            # get the top 10 artists by most additions to playlists
-            print("Here are your top 10 artists by the most addition to playlists:")
-            # get all the playlists owned by a user
-            curs.execute(f"""
-                        SELECT playlistid
-                        FROM listeners_owns_playlist
-                        WHERE userid = %s
-                    """, (listener_id,))
-            playlists = curs.fetchall()
-            # get all the songids in those playlists
+            method = " "
+            while method[0] != "exit":
+                print("Enter {plays} to get your top 10 artist by most plays,\n"
+                      "{additions} for your top 10 artist by most additions to playlists, \nor "
+                      "{combination} for both!\n")
+                print("Enter {exit} to exit your profile page!")
+                method = input()
+                method = method.strip().split()
+                if method[0] == "exit":
+                    print("exiting the profile page!")
+                    break
 
-            # dictionary to store artist counts
-            artist_counts = {}
-            s = 0
-            for playlist in playlists:
-                p += 1
-                curs.execute(f"""
-                            SELECT songid
-                            FROM playlist_has_song
-                            WHERE playlistid = %s
-                        """, (playlist,))
-                songids = curs.fetchall()
-                for songid in songids:
-                    s += 1
-                    curs.execute(f"""
-                                SELECT artist
-                                FROM SONG
-                                WHERE songid = %s
-                            """, (songid,))
-                    artist = curs.fetchone()
+                elif method[0] == "plays":
+                    top_plays(listener_id)
 
-                    # counts instances of artists in the dictionary
-                    if artist in artist_counts:
-                        artist_counts[artist] += 1
-                    else:
-                        artist_counts[artist] = 1
+                elif method[0] == "additions":
+                    top_additions(listener_id)
 
-            # does the sorting to get the top ten
-            top_artists = sorted(artist_counts.items(), key=lambda x: x[1], reverse=True)[:10]
-            j = 0
-            for artist, count in top_artists:
-                j += 1
-                print(f"\t{j}. {artist[0]}: {count} additions to playlists")
-
-            # this is useful for testing purposes to double-check the num of playlist and num of songs are correct
-            print(f"total songs: {s}")
-            print(f"total playlists: {p}")
-
-
-
-
-
-
-
-
+                elif method[0] == "combination":
+                    top_plays(listener_id)
+                    top_additions(listener_id)
 
 
 
